6779515c44dcd8136e3e4e6f8245d39e5a7024cc
cs19b047iittp <116135747+cs19b047iittp@users.noreply.github.com>
2022-10-19 10:16:14 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,54 +3,108 @@ from torch import nn
 
 def kali():
   print ('kali')
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters 
+input_size = 784 # 28x28
+hidden_size = 512
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset 
+train_dataset = torchvision.datasets.MNIST(root='./data', 
+                                           train=True, 
+                                           transform=transforms.ToTensor(),  
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data', 
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                          batch_size=batch_size, 
+                                          shuffle=False)
   
-# Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
-class YourRollNumberNN(nn.Module):
-  pass
-  # ... your code ...
-  # ... write init and forward functions appropriately ...
+class cs19b047NN(nn.Module):
+  def __init__(self, input_size, hidden_size, num_classes):
+    super(NeuralNet, self).__init__()
+    self.input_size = input_size
+    self.l1 = nn.Linear(input_size, hidden_size) 
+    self.relu = nn.ReLU()
+    self.l2 = nn.Linear(hidden_size, num_classes)  
+    
+  def forward(self, x):
+    out = self.l1(x)
+    out = self.relu(out)
+    out = self.l2(out)
+    # no activation and no softmax at the end
+    return out
+
+model = cs19b047NN(input_size, hidden_size, num_classes).to(device)
     
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model(train_data_loader=None, n_epochs=10):
-  model = None
+  model = cs19b047NN(input_size, hidden_size, num_classes).to(device)
 
-  # write your code here as per instructions
-  # ... your code ...
-  # ... your code ...
-  # ... and so on ...
-  # Use softmax and cross entropy loss functions
-  # set model variable to proper object, make use of train_data
-  
-  print ('Returning model... (rollnumber: xx)')
+  # Loss and optimizer
+  criterion = nn.CrossEntropyLoss()
+  optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
   
   return model
+
+# Train the model
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):  
+        # origin shape: [100, 1, 28, 28]
+        # resized: [100, 784]
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 100 == 0:
+            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
 # sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
-  model = None
+    model = cs19b047NN(config)
+    optimizer = torch.optim.SGD(model.parameters(), lr = lr)
+    loss_fn = nn.CrossEntropyLoss()
 
-  # write your code here as per instructions
-  # ... your code ...
-  # ... your code ...
-  # ... and so on ...
-  # Use softmax and cross entropy loss functions
-  # set model variable to proper object, make use of train_data
-  
-  # In addition,
-  # Refer to config dict, where learning rate is given, 
-  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
-  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
-  # You need to create 2d convoution layers as per specification above in each element
-  # You need to add a proper fully connected layer as the last layer
-  
-  # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
-  # HINT: Flatten function can also be used if required
-  return model
-  
-  
-  print ('Returning model... (rollnumber: xx)')
-  
-  return model
+    for e in range(n_epochs):
+        model.train()
+        totalTrainLoss = 0
+        totalValLoss = 0
+        trainCorrect = 0
+        valCorrect = 0
+        for (x, y) in train_data_loader:
+            (x, y) = (x.to(device), y.to(device))
+            pred = model(x)
+            loss = loss_fn(pred, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            totalTrainLoss += loss
+            trainCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    return model
 
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
 def test_model(model1=None, test_data_loader=None):
