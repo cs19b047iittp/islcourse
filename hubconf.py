0ff60b3fcb2c069b5a4e1238f9b2353586487d38
cs19b047iittp <116135747+cs19b047iittp@users.noreply.github.com>
2022-11-21 12:26:40 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -103,9 +103,9 @@ def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None
   # refer to sklearn documentation
   # the cv parameter can change, ie number of folds  
   
-  metrics = ['accuracy'] #the evaluation program can change what metrics to choose
+  metrics = ['accuracy' : metrics.accuracy_score] #the evaluation program can change what metrics to choose
 
-  grid_search_cv = GridSearchCV(model1, param_grid, cv = cv, scoring= metrics, refit=False)
+  grid_search_cv = GridSearchCV(model1, param_grid, cv = cv, scoring='accuracy', refit=False)
   # create a grid search cv object
   # fit the object on X and y input above
   # write your code here...
