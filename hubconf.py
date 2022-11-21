0bb01f090dbebcadefedd183f4ea0854d5c878bc
cs19b047iittp <116135747+cs19b047iittp@users.noreply.github.com>
2022-11-21 11:22:25 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -68,7 +68,7 @@ def get_metrics(model1=None,X=None,y=None):
   # Obtain accuracy, precision, recall, f1score, auc score - refer to sklearn metrics
   pred=model1.predict(X)
   classification_report(y, pred)
-  print(classification_report, classification_report[0])
+  print(classification_report)
   acc, prec, rec, f1, auc = 0,0,0,0,0
   # write your code here...
   return acc, prec, rec, f1, auc
