bbc8dad140ef0e239c67b36a5cfa41663a8a8818
cs19b047iittp <116135747+cs19b047iittp@users.noreply.github.com>
2022-11-21 12:24:07 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -105,7 +105,7 @@ def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None
   
   metrics = ['accuracy'] #the evaluation program can change what metrics to choose
 
-  grid_search_cv = GridSearchCV(model1, param_grid, cv = cv, scoring= metrics)
+  grid_search_cv = GridSearchCV(model1, param_grid, cv = cv, scoring= metrics, refit=False)
   # create a grid search cv object
   # fit the object on X and y input above
   # write your code here...
